c7f423549b171f130ce987cac6a59a8ab475c635
Rest133 <dubrovinanatolii@mail.ru>
2018-11-08 21:01:08 +0300

    First Update

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -2,80 +2,138 @@ package lesson1;
 
 import kotlin.NotImplementedError;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.*;
+
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
      * Сортировка времён
-     *
+     * <p>
      * Простая
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся моменты времени в формате ЧЧ:ММ:СС,
      * каждый на отдельной строке. Пример:
-     *
+     * <p>
      * 13:15:19
      * 07:26:57
      * 10:00:03
      * 19:56:14
      * 13:15:19
      * 00:40:31
-     *
+     * <p>
      * Отсортировать моменты времени по возрастанию и вывести их в выходной файл с именем outputName,
      * сохраняя формат ЧЧ:ММ:СС. Одинаковые моменты времени выводить друг за другом. Пример:
-     *
+     * <p>
      * 00:40:31
      * 07:26:57
      * 10:00:03
      * 13:15:19
      * 13:15:19
      * 19:56:14
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
+     * Трудоемкость равна O(n * log(n)), а ресурсоемкость: R = O(n)
      */
-    static public void sortTimes(String inputName, String outputName) {
-        throw new NotImplementedError();
+    static public void sortTimes(String inputName, String outputName) throws IOException {
+        Scanner sc = new Scanner(new File(inputName));
+        List<String> sortedTimeList = new ArrayList<>();
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine().trim();
+            if (line.matches("^\\d\\d:\\d\\d:\\d\\d\n?$")) sortedTimeList.add(line);
+            else throw new IOException("Неверный формат");
+        }
+        sc.close();
+
+        Collections.sort(sortedTimeList);
+
+        FileWriter newFile = new FileWriter(new File(outputName));
+        for (String line : sortedTimeList) newFile.write(line + "\n");
+        newFile.close();
+
     }
 
     /**
      * Сортировка адресов
-     *
+     * <p>
      * Средняя
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся фамилии и имена жителей города с указанием улицы и номера дома,
      * где они прописаны. Пример:
-     *
+     * <p>
      * Петров Иван - Железнодорожная 3
      * Сидоров Петр - Садовая 5
      * Иванов Алексей - Железнодорожная 7
      * Сидорова Мария - Садовая 5
      * Иванов Михаил - Железнодорожная 7
-     *
+     * <p>
      * Людей в городе может быть до миллиона.
-     *
+     * <p>
      * Вывести записи в выходной файл outputName,
      * упорядоченными по названию улицы (по алфавиту) и номеру дома (по возрастанию).
      * Людей, живущих в одном доме, выводить через запятую по алфавиту (вначале по фамилии, потом по имени). Пример:
-     *
+     * <p>
      * Железнодорожная 3 - Петров Иван
      * Железнодорожная 7 - Иванов Алексей, Иванов Михаил
      * Садовая 5 - Сидоров Петр, Сидорова Мария
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
+     * Трудоемкость равна O(n * log(n)), а ресурсоемкость: R = O(n)
      */
-    static public void sortAddresses(String inputName, String outputName) {
-        throw new NotImplementedError();
+    static public void sortAddresses(String inputName, String outputName) throws IOException {
+        Scanner sc = new Scanner(new File(inputName));
+        List<String> sortedAddresses = new ArrayList<>();
+        TreeMap<String, Set<String>> sortedMapAddress = new TreeMap<>();
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (line.matches("^.+\\s.+\\s*[-]\\s*.+\\s+\\d+$")) sortedAddresses.add(line);
+            else throw new IllegalArgumentException("Неверный формат строки");
+        }
+
+        for (String x : sortedAddresses) {
+            String[] sorted = x.split(" - "); //sorted[0] - Имена,sorted[1] - Адреса
+            if (!sortedMapAddress.containsKey(sorted[1])) {
+                TreeSet<String> names = new TreeSet<>();
+                names.add(sorted[0]);
+                sortedMapAddress.put(sorted[1], names);
+            } else {
+                sortedMapAddress.get(sorted[1]).add(sorted[0]);
+            }
+        }
+
+
+        FileWriter newFile = new FileWriter(new File(outputName));
+        for (String line : sortedMapAddress.keySet()) {
+            List<String> allNames = new ArrayList<>();
+            for (String x : sortedMapAddress.get(line)) {
+                allNames.add(x);
+            }
+            String names = "";
+            for (String x : allNames) {
+                names += x + ", ";
+            }
+            names = names.substring(0, names.length() - 2);
+            newFile.write(line + " - " + names + "\n");
+        }
+        newFile.close();
     }
 
     /**
      * Сортировка температур
-     *
+     * <p>
      * Средняя
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле заданы температуры различных участков абстрактной планеты с точностью до десятых градуса.
      * Температуры могут изменяться в диапазоне от -273.0 до +500.0.
      * Например:
-     *
+     * <p>
      * 24.7
      * -12.6
      * 121.3
@@ -83,11 +141,11 @@ public class JavaTasks {
      * 99.5
      * -12.6
      * 11.0
-     *
+     * <p>
      * Количество строк в файле может достигать ста миллионов.
      * Вывести строки в выходной файл, отсортировав их по возрастанию температуры.
      * Повторяющиеся строки сохранить. Например:
-     *
+     * <p>
      * -98.4
      * -12.6
      * -12.6
@@ -95,19 +153,39 @@ public class JavaTasks {
      * 24.7
      * 99.5
      * 121.3
+     * Трудоемкость равна O(n * log(n)), а ресурсоемкость: R = O(n)
      */
-    static public void sortTemperatures(String inputName, String outputName) {
-        throw new NotImplementedError();
+    static public void sortTemperatures(String inputName, String outputName) throws IOException {
+        Scanner sc = new Scanner(new File(inputName));
+        List<Integer> temperatures = new ArrayList<>();
+
+        while (sc.hasNextLine()) {
+            double oneTemperatureDouble = Double.parseDouble(sc.nextLine()) * 10;
+            int oneTemperature = (int) oneTemperatureDouble;
+            temperatures.add(oneTemperature);
+        }
+        Collections.sort(temperatures);
+
+        FileWriter newFile = new FileWriter(new File(outputName));
+
+        for (int temperature : temperatures) {
+            if (temperature > -10 && temperature < 0)
+                newFile.write("-" + temperature / 10 + "." + Math.abs(temperature % 10) + "\n");
+            else {
+                newFile.write(temperature / 10 + "." + Math.abs(temperature % 10) + "\n");
+            }
+        }
+        newFile.close();
     }
 
     /**
      * Сортировка последовательности
-     *
+     * <p>
      * Средняя
      * (Задача взята с сайта acmp.ru)
-     *
+     * <p>
      * В файле задана последовательность из n целых положительных чисел, каждое в своей строке, например:
-     *
+     * <p>
      * 1
      * 2
      * 3
@@ -115,39 +193,80 @@ public class JavaTasks {
      * 3
      * 1
      * 2
-     *
+     * <p>
      * Необходимо найти число, которое встречается в этой последовательности наибольшее количество раз,
      * а если таких чисел несколько, то найти минимальное из них,
      * и после этого переместить все такие числа в конец заданной последовательности.
      * Порядок расположения остальных чисел должен остаться без изменения.
-     *
+     * <p>
      * 1
      * 3
      * 3
      * 1
      * 2
      * 2
-     * 2
+     * 2 ///true и false - если встречается несколько раз
+     * Трудоемкость равна O(n), а ресурсоемкость: R = O(n)
      */
-    static public void sortSequence(String inputName, String outputName) {
-        throw new NotImplementedError();
+    static public void sortSequence(String inputName, String outputName) throws IOException {
+        Scanner sc = new Scanner(new File(inputName));
+        Map<Integer, Integer> numbers = new HashMap<>();
+        List<Integer> nums = new ArrayList<>();
+        int maxValue = 0;
+        int max = 0;
+
+
+        while (sc.hasNextLine()) {
+            nums.add(Integer.parseInt(sc.nextLine()));
+        }
+        for (int i : nums) {
+            int n = 1;
+            if (numbers.containsKey(i)) {
+                n = numbers.get(i) + 1;
+            }
+            numbers.put(i, n);
+        }
+        for (int x : numbers.keySet()) {
+            if (numbers.get(x) > maxValue) maxValue = numbers.get(x);
+        }
+        for (int x : numbers.keySet()) {
+            if (numbers.get(x) == maxValue) {
+                max = x;
+            }
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) == max) nums.remove(i);
+        }
+        for (; maxValue != 0; ) {
+            nums.add(max);
+            maxValue--;
+        }
+        FileWriter newFile = new FileWriter(new File(outputName));
+        for (Integer numOfNums : nums) {
+            newFile.write(numOfNums.toString() + "\n");
+        }
+        newFile.close();
     }
 
     /**
      * Соединить два отсортированных массива в один
-     *
+     * <p>
      * Простая
-     *
+     * <p>
      * Задан отсортированный массив first и второй массив second,
      * первые first.size ячеек которого содержат null, а остальные ячейки также отсортированы.
      * Соединить оба массива в массиве second так, чтобы он оказался отсортирован. Пример:
-     *
+     * <p>
      * first = [4 9 15 20 28]
      * second = [null null null null null 1 3 9 13 18 23]
-     *
+     * <p>
      * Результат: second = [1 3 4 9 9 13 15 20 23 28]
+     * Трудоемкость равна O(n), а ресурсоемкость: R = O(n)
      */
     static <T extends Comparable<T>> void mergeArrays(T[] first, T[] second) {
-        throw new NotImplementedError();
+        for (int i = 0; i < first.length; i++) {
+            if (second[i] == null) second[i] = first[i];
+        }
+        Arrays.sort(second);
     }
 }
--- src/lesson2/JavaAlgorithms.java
+++ src/lesson2/JavaAlgorithms.java
@@ -3,17 +3,19 @@ package lesson2;
 import kotlin.NotImplementedError;
 import kotlin.Pair;
 
-import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaAlgorithms {
     /**
      * Получение наибольшей прибыли (она же -- поиск максимального подмассива)
      * Простая
-     *
+     * <p>
      * Во входном файле с именем inputName перечислены цены на акции компании в различные (возрастающие) моменты времени
      * (каждая цена идёт с новой строки). Цена -- это целое положительное число. Пример:
-     *
+     * <p>
      * 201
      * 196
      * 190
@@ -22,73 +24,99 @@ public class JavaAlgorithms {
      * 194
      * 193
      * 185
-     *
+     * <p>
      * Выбрать два момента времени, первый из них для покупки акций, а второй для продажи, с тем, чтобы разница
      * между ценой продажи и ценой покупки была максимально большой. Второй момент должен быть раньше первого.
      * Вернуть пару из двух моментов.
      * Каждый момент обозначается целым числом -- номер строки во входном файле, нумерация с единицы.
      * Например, для приведённого выше файла результат должен быть Pair(3, 4)
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
-    static public Pair<Integer, Integer> optimizeBuyAndSell(String inputName) {
-        throw new NotImplementedError();
+    static public Pair<Integer, Integer> optimizeBuyAndSell(String inputName) throws IOException {
+        Scanner sc = new Scanner(new File(inputName));
+        List<Integer> costs = new ArrayList<>();
+        Pair<Integer, Integer> answer = new Pair<>(0, 0);
+        int different = 0;
+        costs.add(0);
+
+        while (sc.hasNextLine()) {
+            costs.add(Integer.parseInt(sc.nextLine()));
+        }
+
+        for (int i = 1; i < costs.size(); i++) {
+            for (int j = i + 1; j < costs.size(); j++) {
+                if (costs.get(j) - costs.get(i) > different) different = costs.get(j) - costs.get(i);
+            }
+        }
+
+        for (int i = 1; i < costs.size(); i++) {
+            for (int j = i + 1; j < costs.size(); j++) {
+                if (costs.get(j) - costs.get(i) == different) answer = new Pair<>(i, j);
+            }
+        }
+
+        return answer;
     }
 
     /**
      * Задача Иосифа Флафия.
      * Простая
-     *
+     * <p>
      * Образовав круг, стоят menNumber человек, пронумерованных от 1 до menNumber.
-     *
+     * <p>
      * 1 2 3
      * 8   4
      * 7 6 5
-     *
+     * <p>
      * Мы считаем от 1 до choiceInterval (например, до 5), начиная с 1-го человека по кругу.
      * Человек, на котором остановился счёт, выбывает.
-     *
+     * <p>
      * 1 2 3
      * 8   4
      * 7 6 х
-     *
+     * <p>
      * Далее счёт продолжается со следующего человека, также от 1 до choiceInterval.
      * Выбывшие при счёте пропускаются, и человек, на котором остановился счёт, выбывает.
-     *
+     * <p>
      * 1 х 3
      * 8   4
      * 7 6 Х
-     *
+     * <p>
      * Процедура повторяется, пока не останется один человек. Требуется вернуть его номер (в данном случае 3).
-     *
+     * <p>
      * 1 Х 3
      * х   4
      * 7 6 Х
-     *
+     * <p>
      * 1 Х 3
      * Х   4
      * х 6 Х
-     *
+     * <p>
      * х Х 3
      * Х   4
      * Х 6 Х
-     *
+     * <p>
      * Х Х 3
      * Х   х
      * Х 6 Х
-     *
+     * <p>
      * Х Х 3
      * Х   Х
      * Х х Х
+     * Трудоемкость:О(n);
      */
     static public int josephTask(int menNumber, int choiceInterval) {
-        throw new NotImplementedError();
+        int g = 0;
+        for (int i = 0; i < menNumber; ++i)
+            g = (g + choiceInterval) % (i + 1);
+        return g + 1;
     }
 
     /**
      * Наибольшая общая подстрока.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например ОБСЕРВАТОРИЯ и КОНСЕРВАТОРЫ.
      * Найти их самую длинную общую подстроку -- в примере это СЕРВАТОР.
      * Если общих подстрок нет, вернуть пустую строку.
@@ -97,45 +125,83 @@ public class JavaAlgorithms {
      * вернуть ту из них, которая встречается раньше в строке first.
      */
     static public String longestCommonSubstring(String firs, String second) {
-        throw new NotImplementedError();
+        List<String> str1 = new ArrayList<>();
+
+        System.out.println(firs);
+        System.out.println(second);
+        System.out.println("/------");
+        for (int i = 1; i < firs.length(); i++) {
+            str1.add(((Character) firs.toCharArray()[i - 1]).toString() + ((Character) firs.toCharArray()[i]).toString());
+        }
+        String[][] matrix = new String[str1.size()][second.length()];
+
+        for (int i = 0; i < str1.size(); i++) {
+            matrix[i][0] = str1.get(i);
+        }
+        for (int i = 0; i < second.length(); i++) {
+            matrix[0][i] = ((Character) second.toCharArray()[i]).toString();
+        }
+
+        for (int i = 0; i < str1.size(); i++) {
+            for (int j = 0; j < second.length(); j++) {
+                System.out.println(matrix[i][j]);
+            }
+            System.out.print("\n");
+        }
+
+        System.out.println("/------");
+        return "";
     }
 
     /**
      * Число простых чисел в интервале
      * Простая
-     *
+     * <p>
      * Рассчитать количество простых чисел в интервале от 1 до limit (включительно).
      * Если limit <= 1, вернуть результат 0.
-     *
+     * <p>
      * Справка: простым считается число, которое делится нацело только на 1 и на себя.
      * Единица простым числом не считается.
+     * Трудоёмкость равна O(N*log(log N)), a ресурсоёмкость: R = O(N)
      */
     static public int calcPrimesNumber(int limit) {
-        throw new NotImplementedError();
+        int answer = 0;
+        if (limit <= 1)
+            return answer;
+        int[] sum = new int[limit + 1];
+
+        for (int i = 2; i <= limit; i++) sum[i] = 1;
+        for (int i = 2; i <= limit; i++) {
+            if (sum[i] == 1) {
+                answer++;
+                for (int j = i; j <= limit; j += i) sum[j] = 0;
+            }
+        }
+        return answer;
     }
 
     /**
      * Балда
      * Сложная
-     *
+     * <p>
      * В файле с именем inputName задана матрица из букв в следующем формате
      * (отдельные буквы в ряду разделены пробелами):
-     *
+     * <p>
      * И Т Ы Н
      * К Р А Н
      * А К В А
-     *
+     * <p>
      * В аргументе words содержится множество слов для поиска, например,
      * ТРАВА, КРАН, АКВА, НАРТЫ, РАК.
-     *
+     * <p>
      * Попытаться найти каждое из слов в матрице букв, используя правила игры БАЛДА,
      * и вернуть множество найденных слов. В данном случае:
      * ТРАВА, КРАН, АКВА, НАРТЫ
-     *
+     * <p>
      * И т Ы Н     И т ы Н
      * К р а Н     К р а н
      * А К в а     А К В А
-     *
+     * <p>
      * Все слова и буквы -- русские или английские, прописные.
      * В файле буквы разделены пробелами, строки -- переносами строк.
      * Остальные символы ни в файле, ни в словах не допускаются.
